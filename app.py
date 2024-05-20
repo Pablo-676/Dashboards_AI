751db50660ed9315a79a6436d4b1367bc13dbe4c
Pablo Vítor <pablovitorrocha017@gmail.com>
2024-05-19 22:34:08 -0300

    start_deploy

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("AI Dashboards")
+
+with st.container():
+    #st.subheader("AI Dashboards")
+    st.title("Pagina Home")
+    st.write("Sejá bem vindo ao AI DashBoards")
+    st.write("Quer acompanhar a jornada de 30 dias com python? siga o meu perfil no link [Clique Aqui](https://www.linkedin.com/in/pablo-almeida-160b21223)")
